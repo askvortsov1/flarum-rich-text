be85c045fb7884314fba965b64ab17eeea97fab1
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-02-22 00:48:39 -0500

    Add a few more exports

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -9,5 +9,7 @@ app.initializers.add('askvortsov/flarum-rich-text', () => {
 export * from './components';
 export * from './proseMirror';
 export * from './states';
+export { InputRule } from 'prosemirror-inputrules';
 export { keymap } from 'prosemirror-keymap';
+export { liftListItem, splitListItem } from 'tiptap-commands';
 export { Plugin, Selection } from 'prosemirror-state';
