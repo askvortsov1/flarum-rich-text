b6bfaba17678a3c24b930270859fca70d2c271cf
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-02-27 22:29:44 -0500

    Export wrapInList

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -12,4 +12,5 @@ export * from './states';
 export { InputRule } from 'prosemirror-inputrules';
 export { keymap } from 'prosemirror-keymap';
 export { liftListItem, splitListItem } from 'tiptap-commands';
+export { wrapInList } from 'prosemirror-schema-list';
 export { Plugin, Selection } from 'prosemirror-state';
